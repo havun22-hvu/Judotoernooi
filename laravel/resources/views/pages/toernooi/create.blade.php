cd6b5b146a14de4fcaa4efb2f66a4eec4ac84784
havun22-hvu <havun22@gmail.com>
2025-12-15 00:41:44 +0100

    Simplify tournament creation form - only name, date, organisation, location

--- laravel/resources/views/pages/toernooi/create.blade.php
+++ laravel/resources/views/pages/toernooi/create.blade.php
@@ -33,30 +33,10 @@
                    class="w-full border rounded px-3 py-2">
         </div>
 
-        <div class="mb-4">
+        <div class="mb-6">
             <label for="locatie" class="block text-gray-700 font-bold mb-2">Locatie</label>
             <input type="text" name="locatie" id="locatie" value="{{ old('locatie') }}"
                    class="w-full border rounded px-3 py-2">
-        </div>
-
-        <div class="grid grid-cols-2 gap-4 mb-4">
-            <div>
-                <label for="aantal_matten" class="block text-gray-700 font-bold mb-2">Aantal Matten</label>
-                <input type="number" name="aantal_matten" id="aantal_matten" value="{{ old('aantal_matten', 7) }}"
-                       class="w-full border rounded px-3 py-2" min="1" max="20">
-            </div>
-            <div>
-                <label for="aantal_blokken" class="block text-gray-700 font-bold mb-2">Aantal Blokken</label>
-                <input type="number" name="aantal_blokken" id="aantal_blokken" value="{{ old('aantal_blokken', 6) }}"
-                       class="w-full border rounded px-3 py-2" min="1" max="12">
-            </div>
-        </div>
-
-        <div class="mb-6">
-            <label for="verwacht_aantal_judokas" class="block text-gray-700 font-bold mb-2">Verwacht aantal judoka's</label>
-            <input type="number" name="verwacht_aantal_judokas" id="verwacht_aantal_judokas" value="{{ old('verwacht_aantal_judokas', 300) }}"
-                   class="w-full border rounded px-3 py-2" min="10" max="2000">
-            <p class="text-gray-500 text-sm mt-1">Schatting voor planning (kan later aangepast worden)</p>
         </div>
 
         <div class="flex justify-end space-x-4">
