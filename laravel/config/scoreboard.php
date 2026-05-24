7517f29063c2fcbaa6bcf027b7d94640366e54db
havun22-hvu <havun22@gmail.com>
2026-05-24 21:12:49 +0200

    fix(scoreboard): bump default version_code to 106

--- laravel/config/scoreboard.php
+++ laravel/config/scoreboard.php
@@ -2,7 +2,7 @@
 
 return [
     'version' => env('SCOREBOARD_VERSION', '1.0.0'),
-    'version_code' => env('SCOREBOARD_VERSION_CODE', 100),
+    'version_code' => env('SCOREBOARD_VERSION_CODE', 106),
     'download_url' => env('SCOREBOARD_DOWNLOAD_URL', ''),
     'force_update' => env('SCOREBOARD_FORCE_UPDATE', false),
     'release_notes' => env('SCOREBOARD_RELEASE_NOTES', ''),
